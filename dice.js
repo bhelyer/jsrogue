14f5f0b663c660c19a35ec49a9de4e58a1450b63
Bernard Helyer <b.helyer@gmail.com>
2015-01-18 19:03:55 +1300

    dice: Allow plain numbers and low-high syntax.

--- dice.js
+++ dice.js
@@ -1,29 +1,45 @@
 /*
- * Creates a die or dice with a given die string.
+ * Creates an object that returns a random number.
+ *   '4' returns 4.
+ *   '10-15' returns a value from 10 to 15 inclusive.
  *   'd6' or '1d6'  one six sided die, 1-6
  *   '2d3' two three sided dice 1-3 + 1-3
  *   '1d20+2' one twenty sided die, +2 to the result.
  *   '3d6-5' three six sided dice, -5 to the result. (1-6 + 1-6 + 1-6) - 5
  */
 function Dice(str) {
-	var d = new Object();
 	var strs = str.split("d");
+	if (strs.length === 1) {
+		dashes = str.split("-");
+		if (dashes.length <= 1) {
+			this.val = parseInt(dashes[0], 10);
+			this.roll = function() { return this.val; };
+			return;
+		} else {
+			this.lower = parseInt(dashes[0], 10);
+			this.upper = parseInt(dashes[1], 10);
+			this.roll = simpleRoll;
+			return;
+		}
+	}
 	if (strs[0].length === 0) {
 		strs[0] = "1";
 	}
-	d.numberOfDice = parseInt(strs[0], 10);
+	this.numberOfDice = parseInt(strs[0], 10);
 	if (strs[1].indexOf("+") != -1) {
-		d.modifier = parseInt(strs[1].substr(strs[1].indexOf("+")+1), 10);
+		this.modifier = parseInt(strs[1].substr(strs[1].indexOf("+")+1), 10);
 	} else if (strs[1].indexOf("-") != -1) {
-		d.modifier = parseInt(strs[1].substr(strs[1].indexOf("-")), 10);
+		this.modifier = parseInt(strs[1].substr(strs[1].indexOf("-")), 10);
 	} else {
-		d.modifier = 0;
+		this.modifier = 0;
 	}
-	d.sides = parseInt(strs[1], 10);
+	this.sides = parseInt(strs[1], 10);
 
-	d.roll = diceRoll;
+	this.roll = diceRoll;
+}
 
-	return d;
+function simpleRoll() {
+	return getRandomInt(this.lower, this.upper + 1);
 }
 
 function diceRoll() {
